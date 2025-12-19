5537cdae7f429de55f5cd7df90ef338eb57c7c14
Florian Rohrer <semicolonator@gmail.com>
2025-12-19 21:03:17 +0100

    survival

--- scripts/survial_kaplan_meier.R
+++ scripts/survial_kaplan_meier.R
@@ -5,6 +5,14 @@ library(tidyr)
 library(dplyr)
 library(patchwork)
 library(pammtools)
+library(showtext)
+library(sysfonts)
+
+
+font_add_google("Roboto Condensed")
+showtext_auto()
+showtext_opts(dpi = 100)
+
 
 fit <- survfit(Surv(time, status) ~ sex, data = lung)
 df <- tibble(
@@ -46,7 +54,7 @@ df <- tibble(
     strata = factor(
       strata,
       levels = c("sex=1", "sex=2"),
-      labels = c("male", "female")
+      labels = c("Men", "Women")
     )
   ) |>
   mutate(time_months = time_days / 30.44)
@@ -55,9 +63,30 @@ df_censor <- df |>
   filter(n.censor > 0)
 
 label_data <- tibble(
-  strata = factor(c("male", "female"), levels = c("male", "female")),
+  strata = factor(c("Men", "Women"), levels = c("Men", "Women")),
   time_months = c(3, 16.5),
   surv = c(0.65, 0.65)
+)
+
+df_median <- surv_median(fit) |>
+  as_tibble() |>
+  mutate(
+    strata = factor(
+      strata,
+      levels = c("sex=1", "sex=2"),
+      labels = c("Men", "Women")
+    ),
+    median_time = median / 30.44 # convert days to months
+  )
+
+median_text <- paste0(
+  "Median Survival:\n",
+  "Men = ",
+  round(df_median$median_time[df_median$strata == "Men"], 1),
+  " months\n",
+  "Women = ",
+  round(df_median$median_time[df_median$strata == "Women"], 1),
+  " months"
 )
 
 p1 <- ggplot(
@@ -67,7 +96,6 @@ p1 <- ggplot(
   geom_stepribbon(
     aes(ymin = lower, ymax = upper),
     alpha = 0.3,
-    color = NA,
   ) +
   geom_step(linewidth = 1.2) +
   geom_point(
@@ -81,25 +109,56 @@ p1 <- ggplot(
   geom_text(
     data = label_data,
     aes(x = time_months, y = surv, label = strata, color = strata),
-    fontface = "bold",
-    size = 4
+    # fontface = "bold",
+    family = "Roboto Condensed",
+    size = 5
   ) +
-  labs(x = "Time [Months]", y = "Survival Probability") +
-  scale_color_manual(values = c("female" = "#ef8a62", "male" = "#67a9cf")) +
-  scale_fill_manual(values = c("female" = "#ef8a62", "male" = "#67a9cf")) +
+  geom_hline(
+    yintercept = 0.5,
+    color = "black",
+    linetype = "dashed",
+  ) +
+  geom_label(
+    x = -1,
+    y = 0.45,
+    color = "black",
+    family = "Roboto Condensed",
+    fill = "white",
+    hjust = 0,
+    label = median_text,
+    label.size = NA,
+    size = 4.5,
+    vjust = 1,
+  ) +
+  labs(x = "Time [Months]", y = "Survival probability") +
+  scale_color_manual(values = c("Women" = "#ef8a62", "Men" = "#67a9cf")) +
+  scale_fill_manual(values = c("Women" = "#ef8a62", "Men" = "#67a9cf")) +
   scale_x_continuous(
     breaks = seq(0, max(df$time_months), by = 6),
   ) +
+  scale_y_continuous(
+    breaks = seq(0, 1, by = 0.25),
+    labels = scales::number_format(accuracy = 0.01),
+  ) +
   theme_minimal() +
   theme(
+    axis.line = element_line(linewidth = 0.75),
+    axis.ticks = element_line(color = "black", linewidth = 0.5),
+    axis.ticks.length = unit(0.1, "cm"),
+    axis.text = element_text(
+      size = 12,
+      color = "black",
+    ),
+    axis.title = element_text(size = 14, color = "black"),
     legend.position = "none",
     panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
     panel.grid.major.x = element_line(
       color = "gray90",
       linewidth = 0.3,
-      linetype = "solid"
+      linetype = "solid",
     ),
+    panel.grid.minor = element_blank(),
+    text = element_text(family = "Roboto Condensed"),
   )
 
 risk_data <- df |>
@@ -131,7 +190,7 @@ p2 <- ggplot(
   aes(x = time_months, y = strata, label = label, color = strata)
 ) +
   geom_text(size = 3.5, fontface = "bold") +
-  scale_color_manual(values = c("female" = "#ef8a62", "male" = "#67a9cf")) +
+  scale_color_manual(values = c("Women" = "#ef8a62", "Men" = "#67a9cf")) +
   scale_x_continuous(
     breaks = seq(0, max(df$time_months), by = 6),
     limits = c(0, max(df$time_months))
