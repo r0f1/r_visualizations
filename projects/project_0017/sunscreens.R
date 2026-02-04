1922879a688c9d7b84d5ec9a9acd56a741701fb3
Florian Rohrer <semicolonator@gmail.com>
2026-02-04 21:23:09 +0100

    sunscreen

--- projects/project_0017/sunscreens.R
+++ projects/project_0017/sunscreens.R
@@ -45,11 +45,20 @@ data <- sunscreens |>
   )
 
 
-color_claim = "grey20"
-color_okay = "#19647E"
-color_not_okay = "#C73E1D"
+color_claim = "#807219"
+color_claim_font = "#4d440f"
+color_measured = "#FF9505"
+color_measured_font = "#cc7704"
 
 p <- ggplot(data, aes(x = Measured_SPF, y = display_name)) +
+  geom_segment(
+    data = data,
+    aes(y = display_name, yend = display_name),
+    x = -2,
+    xend = 82,
+    color = "grey90",
+    linewidth = 0.25,
+  ) +
   geom_segment(
     aes(xend = claimed_spf, yend = display_name),
     linewidth = 0.5
@@ -66,10 +75,11 @@ p <- ggplot(data, aes(x = Measured_SPF, y = display_name)) +
       hjust = claimed_hjust
     ),
     size = 3,
-    color = color_claim
+    color = color_claim_font,
   ) +
   geom_point(
-    aes(x = Measured_SPF, color = Meets_Claim),
+    aes(x = Measured_SPF),
+    color = color_measured,
     size = 3
   ) +
   geom_text(
@@ -77,72 +87,124 @@ p <- ggplot(data, aes(x = Measured_SPF, y = display_name)) +
       x = Measured_SPF,
       label = sprintf("%.0f", Measured_SPF),
       hjust = measured_hjust,
-      color = Meets_Claim,
     ),
+    color = color_measured_font,
     size = 3,
   ) +
   geom_text(
-    aes(x = 5, label = Brand),
+    aes(x = -5, label = Brand),
     hjust = 1,
     fontface = "bold",
     family = "Roboto Condensed",
-    nudge_y = 0.15,
+    nudge_y = 0.2,
     size = 3.5
   ) +
   geom_text(
-    aes(x = 5, label = Product_Name),
+    aes(x = -5, label = stringr::str_wrap(Product_Name, width = 40)),
     hjust = 1,
     fontface = "plain",
     family = "Roboto Condensed",
-    nudge_y = -0.25,
+    nudge_y = -0.2,
     size = 3,
     lineheight = 0.9
   ) +
-  scale_color_manual(
-    values = c("No" = color_not_okay, "Yes" = color_okay)
+  annotate(
+    "text",
+    x = 26,
+    y = 20.5,
+    label = "Measured SPF",
+    hjust = 0.5,
+    color = color_measured,
+    size = 3,
+    fontface = "bold",
+  ) +
+  annotate(
+    "text",
+    x = 50,
+    y = 20.5,
+    label = "Claimed SPF",
+    hjust = 0.5,
+    color = color_claim,
+    size = 3,
+    fontface = "bold",
+  ) +
+  annotate(
+    "text",
+    x = 80,
+    y = 15,
+    label = stringr::str_wrap(
+      "Only 4 / 20 tested products reached or exceeded their claims.",
+      width = 20
+    ),
+    hjust = 1,
+    color = "grey20",
+    size = 3,
   ) +
   scale_x_continuous(
     breaks = seq(0, 80, by = 10),
-    limits = c(0, 80),
+    limits = c(-40, 80),
     expand = c(0.05, 0),
+    oob = scales::squish,
   ) +
-  scale_y_discrete(labels = identity) +
   coord_cartesian(clip = "off") +
   labs(
-    title = "Sunscreen SPF Claims vs Measured Results",
+    title = glue::glue(
+      "<span style='color:{color_claim};'>Claimed</span> vs <span style='color:{color_measured};'>Measured</span> Sun Protection Factor"
+    ),
     subtitle = "16 out of 20 sunscreens failed to meet their SPF claims",
     x = NULL,
     y = NULL,
+    caption = social_caption,
   ) +
   theme_minimal() +
   theme(
     axis.text.y = element_blank(),
     axis.title.y = element_blank(),
     legend.position = "none",
-    panel.grid.minor.x = element_blank(),
     panel.grid.major.x = element_line(
       color = "grey90",
       size = 0.25,
       linetype = "solid",
     ),
-    panel.grid.major.y = element_line(
-      color = "grey90",
-      size = 0.25,
-      linetype = "solid",
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    plot.caption = ggtext::element_markdown(
+      size = 10,
+      color = "grey40",
+      margin = margin(t = 20),
     ),
     plot.margin = margin(l = 40, r = 15, t = 15, b = 15),
-    # plot.title = element_text(face = "bold", size = 14),
-    # plot.subtitle = element_text(size = 10, color = "gray40"),
+    plot.title = ggtext::element_textbox_simple(
+      face = "bold",
+      size = 20,
+      hjust = 0,
+      margin = margin(l = -5, b = 10),
+      lineheight = 1.2,
+    ),
+    plot.subtitle = element_text(size = 10, color = "gray40"),
     text = element_text(family = "Roboto"),
+  ) +
+  patchwork::plot_annotation(
+    caption = "Source: CHOICE",
+    theme = theme(
+      plot.caption = element_text(
+        size = 10,
+        hjust = 0,
+        margin = margin(t = -25),
+        color = "grey40",
+      ),
+      text = element_text(family = "Roboto Condensed"),
+    )
   )
 
-p
 
-# ggsave(
-#   here::here("projects", "project_0017", "sunscreens.svg"),
-#   plot = p,
-#   width = 650,
-#   height = 750,
-#   units = "px",
-#   dpi = 100,
-# )
+# p
+
+ggsave(
+  here::here("projects", "project_0017", "sunscreens.svg"),
+  plot = p,
+  width = 650,
+  height = 1000,
+  units = "px",
+  dpi = 100,
+)
